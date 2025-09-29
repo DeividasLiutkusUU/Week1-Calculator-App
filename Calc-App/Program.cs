673271591ad364f7629b707ade5b14cf56b0005b
Deividas Liutkus <liutkus-d@ulster.ac.uk>
2025-09-29 14:29:06 +0100

    Add project files.

---
+++ Calc-App/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Input the firsr value");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input the second value");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1}The answer is {2}", firstNumber, secondNumber, result);
+Console.WriteLine();
